e3a2a1be73f082048e2b765f704f2bdfa529eed2
igorek19882020 <github.com/igorek19882020/Igor>
2020-02-06 00:29:20 +0300

    Initial commit

---
+++ src/Minsk/Samost/R/SixFebr2020/Task.java
@@ -0,0 +1,14 @@
+package Minsk.Samost.R.SixFebr2020;
+
+public class Task {
+    public static void main(String[] args) {
+       int mT[][] = new int[10][10];
+               for (int i = 0;i<10;i++){
+                   for(int j=  0;j<10;j++){
+                       mT[i][j] = (i+1)*(j+1);
+                       System.out.printf("%7d", mT[i][j]);
+                   }
+                   System.out.println();
+               }
+    }
+}
