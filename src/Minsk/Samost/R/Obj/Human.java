8e3b3b4e4768248bfabbca23d5c9125b6e2b3999
igorek19882020 <github.com/igorek19882020/Igor>
2020-02-11 17:00:05 +0300

    1

---
+++ src/Minsk/Samost/R/Obj/Human.java
@@ -0,0 +1,22 @@
+package Minsk.Samost.R.Obj;
+
+public class Human {
+    int age;
+    String name;
+
+    public Human(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public String toString(){
+        return name + " "+age;
+    }
+}
+
+
+
+
+
+
+
---
+++ src/Minsk/Samost/R/Obj/Main.java
@@ -0,0 +1,10 @@
+package Minsk.Samost.R.Obj;
+
+public class Main {
+    public static void main(String[] args) {
+       Human h=  new Human(34,"Bob");
+       h.toString();
+
+       System.out.println(h);
+    }
+}
